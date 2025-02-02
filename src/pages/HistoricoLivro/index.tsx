fdf7814ce76e723544bc03e1022b00beb58422ec
Guilherme Zanella <guilhermeczanella@gmail.com>
2025-02-02 20:42:35 -0300

    feat: routes US09 emprestimo de livros Co-authored-by:  Jonas Carlos <jonascarlos.na@gmail.com>

---
+++ src/pages/HistoricoLivro/index.tsx
@@ -0,0 +1,14 @@
+import { Box, Flex } from '@chakra-ui/react';
+import { SideBar } from '../../components/SideBar';
+
+function HistoricoLivro() {
+  return (
+    <Box height='100vh'>
+      <Flex height='100%'>
+        <SideBar />
+      </Flex>
+    </Box>
+  );
+}
+
+export default HistoricoLivro
